10ca4f056e46e929e5fef1e5e53e4252f821b4b1
msifatsadiq <msifatsadiq@gmail.com>
2024-03-11 22:02:21 +0600

    adding the flags, name, area, population etc and work on some button validition

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
     // console.log(country);
-    const { name,flags } = country;
+    const { name, flags, population, area, region } = country;
+
+    const [visited, setVisited] = useState(false)
+
+    const handelVisited = () => {
+        setVisited(!visited)
+    }
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>Language: {region}</p>
+            <p>{name.official}</p>
+            <button onClick={handelVisited}>{visited ? 'going':'Not'}</button>
+            {visited ? 'I have visited this country ' : 'i want to visit'}
         </div>
     );
 };
